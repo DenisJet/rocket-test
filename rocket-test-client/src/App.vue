e853d6de4d67b1276847aa25c643af4830c17752
Denis Kalkopf <deniskalkopf@gmail.com>
2024-05-30 14:49:40 +0500

    add search

--- rocket-test-client/src/App.vue
+++ rocket-test-client/src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div style="background: #ececec; padding: 30px;">
+  <div style="padding: 30px;">
     <a-card title="Тестовое задание" :bordered="false" style="width: 100%">
+      <template #extra><a-input-search v-model:value="value" placeholder="Найти..." style="width: 200px" 
+        @search="onSearch"
+    /></template> 
       <a-table :columns="columns" :data-source="data" :scroll="{ x: 900 }" :expand-column-width="50" :pagination="false">
         <template #expandedRowRender="{ record }">
           <p style="margin: 0">
@@ -11,7 +14,17 @@
     </a-card>
   </div>
 </template>
+
 <script lang="ts" setup>
+
+import { ref } from 'vue';
+const value = ref<string>('');
+
+const onSearch = (searchValue: string) => {
+  console.log('use value', searchValue);
+  console.log('or use this.value', value.value);
+};
+
 const columns = [
   { title: 'Название', dataIndex: 'name', key: 'name', fixed: true },
   { title: 'Бюджет', dataIndex: 'budget', key: 'budget' },
